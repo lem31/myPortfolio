8d93025560518d3b45c4749df818928889b83d3e
Leanne <lemeyer.no@gmail.com>
2025-06-03 20:06:46 +0200

    initial commit

---
+++ src/Components/Router/index.jsx
@@ -0,0 +1,59 @@
+/**
+ * Router component that sets up the application's routing structure using React Router.
+ *
+ * @component
+ * @returns {JSX.Element} The Router component with defined routes for the application.
+ *
+ * Routes:
+ * - "/" renders the Layout component with nested routes:
+ *   - index route renders MyPortfolio
+ *   - "/ProjectOne" renders ProjectOne
+ *   - "/ProjectTwo" renders ProjectTwo
+ *   - "/ProjectThree" renders ProjectThree
+ *   - "/About" renders About
+ *   - "/Contact" renders Contact
+ */
+
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+
+function Router() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={<Layout />}
+          >
+            <Route
+              index
+              element={<MyPortfolio />}
+            />
+            <Route
+              path="/ProjectOne"
+              element={<ProjectOne />}
+            />
+            <Route
+              path="/ProjectTwo"
+              element={<ProjectTwo />}
+            />
+            <Route
+              path="/ProjectThree"
+              element={<ProjectThree />}
+            />
+            <Route
+              path="/About"
+              element={<About />}
+            />
+            <Route
+              path="/Contact"
+              element={<Contact />}
+            />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default Router;
