54ffdce8afd73cb4672af241a3f047d93a803e13
Leanne <lemeyer.no@gmail.com>
2025-06-04 19:39:22 +0200

    added more styles to portfolio page and added personal info section

--- src/Components/Pages/MyPortfolio/index.jsx
+++ src/Components/Pages/MyPortfolio/index.jsx
@@ -2,6 +2,8 @@ import ProjectCard from "../../ProjectCard";
 import HolidazeMainImage from "../../../assets/Images/HolidazeMainImage.jpg";
 import StoreMainImage from "../../../assets/Images/StoreMainImg.png";
 import AuctionMainImage from "../../../assets/Images/AuctionAppMainImg.png";
+import ProfileCard from "../../ProfileCard";
+import portfolioStyles from "../../../CSS_Modules/Portfolio/index.module.css";
 
 const projects = [
   { title: "Holidaze App", image: HolidazeMainImage, link: "/ProjectOne" },
@@ -11,11 +13,16 @@ const projects = [
 
 const PortfolioPage = () => {
   return (
+    <div  className={portfolioStyles.portfolioPageDiv}>
+    <ProfileCard />
+
+
     <div style={{ display: "flex", flexDirection: 'row', gap: "20px", justifyContent: "center", width: "100%", maxWidth:'1260px', flexWrap: 'wrap', paddingTop: "120px", margin:'0 auto' }}>
       {projects.map((project, index) => (
         <ProjectCard key={index} {...project} />
       ))}
     </div>
+     </div>
   );
 };
 
---
+++ src/Components/ProfileCard/index.jsx
@@ -0,0 +1,12 @@
+import profileVideo from '../../assets/Videos/ProfileCardVid.mp4';
+import portfolioStyles from '../../CSS_Modules/Portfolio/index.module.css';
+
+const ProfileCard = () => {
+  return (
+<div>
+<video className={portfolioStyles.video} autoPlay muted src={profileVideo} />
+</div>
+  );
+};
+
+export default ProfileCard;
