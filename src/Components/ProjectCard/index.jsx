f150fdeb2911067cf3f8cb0eb521fee3bff32555
Leanne <lemeyer.no@gmail.com>
2025-06-05 21:59:26 +0200

    added any missing jsdocs

--- src/Components/ProjectCard/index.jsx
+++ src/Components/ProjectCard/index.jsx
@@ -1,3 +1,15 @@
+/**
+ * ProjectCard component displays a project as a clickable card with an image, title, and description.
+ * On hover, it shows an enlarged image overlay.
+ *
+ * @component
+ * @param {Object} props - Component props
+ * @param {string} props.title - The title of the project
+ * @param {string} props.image - The image URL for the project
+ * @param {string} props.link - The route to navigate to when the card is clicked
+ * @param {string} props.description - A short description of the project
+ * @returns {JSX.Element} The rendered ProjectCard component
+ */
 import { useState } from "react";
 import { Card, CardMedia, CardContent, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
@@ -6,52 +18,59 @@ import globStyles from "../../CSS_Modules/Global/index.module.css";
 
 const ProjectCard = ({ title, image, link, description }) => {
   const navigate = useNavigate();
-    const [hovered, setHovered] = useState(false);
+  const [hovered, setHovered] = useState(false);
 
   return (
     <>
-    <Card
-   className={portfolioStyles.projectCard}
-      onClick={() => navigate(link)}
+      <Card
+        className={portfolioStyles.projectCard}
+        onClick={() => navigate(link)}
+      >
+        <div className={portfolioStyles.cardMediaDiv}>
+          <CardMedia
+            component="img"
+            image={image}
+            alt={title}
+            sx={{
+              width: "100%",
+              height: "100%",
 
-    >
-      <div className={portfolioStyles.cardMediaDiv}>
-        <CardMedia
-          component="img"
-          image={image}
-          alt={title}
-          sx={{
-            width: "100%",
-            height: "100%",
+              objectFit: "contain",
+              margin: "0 auto",
+              transition: "transform 0.3s ease-in-out",
 
-            objectFit: "contain",
- margin: "0 auto",
-            transition: "transform 0.3s ease-in-out",
-
-            display: "block",
-          }}
-           onMouseEnter={() => setHovered(true)}
-        onMouseLeave={() => setHovered(false)}
-        />{" "}
-      </div>
-      <CardContent>
-        <Typography className={globStyles.h2} sx={{fontWeight:'500'}} >{title}</Typography>
-        <Typography className={globStyles.p} sx={{fontWeight:'400'}} > {description}</Typography>
-
-      </CardContent>
-    </Card>
-{hovered && (
-        <div
-          className={portfolioStyles.cardImgDiv}
-        >
-            <img
+              display: "block",
+            }}
+            onMouseEnter={() => setHovered(true)}
+            onMouseLeave={() => setHovered(false)}
+          />{" "}
+        </div>
+        <CardContent>
+          <Typography
+            className={globStyles.h2}
+            sx={{ fontWeight: "500" }}
+          >
+            {title}
+          </Typography>
+          <Typography
+            className={globStyles.p}
+            sx={{ fontWeight: "400" }}
+          >
+            {" "}
+            {description}
+          </Typography>
+        </CardContent>
+      </Card>
+      {hovered && (
+        <div className={portfolioStyles.cardImgDiv}>
+          <img
             src={image}
             alt={title}
             className={portfolioStyles.cardImg}
           />
         </div>
-)}
-</>
+      )}
+    </>
   );
 };
 
